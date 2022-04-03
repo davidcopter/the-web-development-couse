9e6397cffc0f529e1379272f4d232a843fb62506
Phanuwat Phalad <phanuwat.davidcopter@gmail.com>
2022-04-03 13:29:04 +0700

    loop

--- script.js
+++ script.js
@@ -52,23 +52,43 @@ console.log('Starting script...');
 //   // i++, 0+1=1, 1+1=2, 2+1=3,.., 4+1=5
 // }
 
-const prices = [15, 20, 25, 10];
+// const prices = [15, 20, 25, 10];
 // prices.length = 4 element
 
-let totalPrice = 0;
+// let totalPrice = 0;
 
-// for (let i = 0; i < 4; i++)
-for (let i = 0; i < prices.length; i++) {
-  // i: 0,1,2,3,4(x)
-  // totalPrice: 0,15,35,60
-  totalPrice = totalPrice + prices[i];
-  // (1) totalPrice = 0 +  prices[0] => 0 + 15 = 15
-  // (2) totalPrice = 15 + prices[1] => 15 + 20 => 35
-  // (3) totalPrice = 35 + prices[2] => 35 + 25 => 60
-  // (4) totalPrice = 60 + prices[3] => 60 + 10 =>70
+// 1. for (let i = 0; i < 4; i++)
+// for (let i = 0; i < prices.length; i++) {
+//   // i: 0,1,2,3,4(x)
+//   // totalPrice: 0,15,35,60
+//   totalPrice = totalPrice + prices[i];
+//   // (1) totalPrice = 0 +  prices[0] => 0 + 15 = 15
+//   // (2) totalPrice = 15 + prices[1] => 15 + 20 => 35
+//   // (3) totalPrice = 35 + prices[2] => 35 + 25 => 60
+//   // (4) totalPrice = 60 + prices[3] => 60 + 10 =>70
 
-  console.log('Item = ', prices[i]);
-  console.log('Current TotalPrice = ', totalPrice);
+//   console.log('Item = ', prices[i]);
+//   console.log('Current TotalPrice = ', totalPrice);
 
-  // i++: 1,2,3,4
+//   // i++: 1,2,3,4
+// }
+
+// 2. while loop
+let count = 0;
+// count < 10; 3 < 10 true
+// while (true)
+// ...
+// count < 10; 10 < 10 false
+// while (false)
+while (count < 3) {
+  console.log('Count =', count);
+  count++;
 }
+
+// 3 dowhile *
+let value = true;
+do {
+  console.log('value : ', value);
+  value = false;
+  // while (false)
+} while (value);
